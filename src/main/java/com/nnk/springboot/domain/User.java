551687e129f4abbb16d461953fa7654b72c95ef8
WebisBrian <brian.t.github@gmail.com>
2026-03-24 15:09:12 +0100

    Fix: lombok causing issue with user class -> remove @Setter(AccessLevel = None)

--- src/main/java/com/nnk/springboot/domain/User.java
+++ src/main/java/com/nnk/springboot/domain/User.java
@@ -3,20 +3,17 @@ package com.nnk.springboot.domain;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.*;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Entity
 @Table(name = "users")
-@Getter
-@Setter
-@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 @ToString(of = {"id", "username", "fullname", "role"})
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Setter(AccessLevel.NONE)
     private Integer id;
 
     @NotBlank(message = "Username is mandatory")
@@ -35,10 +32,27 @@ public class User {
     @NotBlank(message = "Role is mandatory")
     private String role;
 
+    public User() {}
+
     public User(String username, String password, String fullname, String role) {
         this.username = username;
         this.password = password;
         this.fullname = fullname;
         this.role = role;
     }
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getFullname() { return fullname; }
+    public void setFullname(String fullname) { this.fullname = fullname; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 }
