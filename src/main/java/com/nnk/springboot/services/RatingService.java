2ce95b3f15bb68fe0efec5ae4db39aa1a809e6b9
WebisBrian <brian.t.github@gmail.com>
2026-03-24 16:19:05 +0100

    Feat: implements RatingService

---
+++ src/main/java/com/nnk/springboot/services/RatingService.java
@@ -0,0 +1,35 @@
+package com.nnk.springboot.services;
+
+import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.repositories.RatingRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RatingService {
+
+    private final RatingRepository ratingRepository;
+
+    public RatingService(RatingRepository ratingRepository) {
+        this.ratingRepository = ratingRepository;
+    }
+
+    public List<Rating> findAll() {
+        return ratingRepository.findAll();
+    }
+
+    public Rating save(Rating rating) {
+        return ratingRepository.save(rating);
+    }
+
+    public Rating findById(Integer id) {
+        return ratingRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Rating not found for id: " + id));
+    }
+
+    public void delete(Integer id) {
+        Rating rating = findById(id);
+        ratingRepository.delete(rating);
+    }
+}
