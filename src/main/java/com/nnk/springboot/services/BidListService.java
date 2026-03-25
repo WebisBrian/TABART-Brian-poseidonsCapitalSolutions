c74e02b51a6b9f775619b5a9e2b7320f0c4df57e
WebisBrian <brian.t.github@gmail.com>
2026-03-25 09:51:29 +0100

    Feat: implements BidListService

---
+++ src/main/java/com/nnk/springboot/services/BidListService.java
@@ -0,0 +1,35 @@
+package com.nnk.springboot.services;
+
+import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.repositories.BidListRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BidListService {
+
+    private final BidListRepository bidListRepository;
+
+    public BidListService(BidListRepository bidListRepository) {
+        this.bidListRepository = bidListRepository;
+    }
+
+    public List<BidList> findAll() {
+        return bidListRepository.findAll();
+    }
+
+    public BidList save(BidList bidList) {
+        return bidListRepository.save(bidList);
+    }
+
+    public BidList findById(Integer id) {
+        return bidListRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("BidList not found for id: " + id));
+    }
+
+    public void delete(Integer id) {
+        BidList bidList = findById(id);
+        bidListRepository.delete(bidList);
+    }
+}
