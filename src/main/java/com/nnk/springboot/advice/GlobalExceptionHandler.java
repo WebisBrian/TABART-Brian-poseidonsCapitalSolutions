21cb32b16a26d4dbcb1506ae23db968c8959b244
WebisBrian <brian.t.github@gmail.com>
2026-03-25 15:17:36 +0100

    Feat: implements GlobalExceptionHandler

---
+++ src/main/java/com/nnk/springboot/advice/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.nnk.springboot.advice;
+
+import com.nnk.springboot.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleResourceNotFoundException(ResourceNotFoundException ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "error";
+    }
+}
