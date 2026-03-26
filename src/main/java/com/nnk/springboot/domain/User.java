4ffd307c9b15b675e537526ab37670de3a71c78c
WebisBrian <brian.t.github@gmail.com>
2026-03-26 13:20:17 +0100

    Refacto: remove pwd Pattern validation in entity -> replace in DTO

--- src/main/java/com/nnk/springboot/domain/User.java
+++ src/main/java/com/nnk/springboot/domain/User.java
@@ -2,7 +2,6 @@ package com.nnk.springboot.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -26,11 +25,6 @@ public class User {
     private String username;
 
     @NotBlank(message = "Password is mandatory")
-    @Pattern(
-            regexp = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$",
-            message = "Password must be at least 8 characters and include an uppercase letter, a digit, and a special character"
-    )
-
     @Column(nullable = false, length = 125)
     private String password;
 
