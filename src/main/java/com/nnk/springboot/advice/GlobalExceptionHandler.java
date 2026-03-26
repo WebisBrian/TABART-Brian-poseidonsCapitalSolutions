b28665ad556b014ca5c11b363e1a69494d085163
WebisBrian <brian.t.github@gmail.com>
2026-03-26 11:05:54 +0100

    Fix: render good template / Refacto: GEH now handle AccessDeniedException

--- src/main/java/com/nnk/springboot/advice/GlobalExceptionHandler.java
+++ src/main/java/com/nnk/springboot/advice/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.nnk.springboot.advice;
 
 import com.nnk.springboot.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,6 +15,13 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleResourceNotFoundException(ResourceNotFoundException ex, Model model) {
         model.addAttribute("errorMessage", ex.getMessage());
-        return "error";
+        return "error/error";
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public String handleAccessDeniedException(Model model) {
+        model.addAttribute("errorMsg", "You are not authorized for the requested data.");
+        return "error/403";
     }
 }
