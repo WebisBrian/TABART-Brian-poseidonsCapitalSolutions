b6368d476b9de607c166bdbfb5b529146e56cca8
WebisBrian <brian.t.github@gmail.com>
2026-03-26 10:40:14 +0100

    Feat: when logged, refuse access to /login -> redirect to /

--- src/main/java/com/nnk/springboot/controllers/LoginController.java
+++ src/main/java/com/nnk/springboot/controllers/LoginController.java
@@ -1,11 +1,22 @@
 package com.nnk.springboot.controllers;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
+
+    @GetMapping("/login")
+    public String login(Authentication authentication) {
+        if (authentication != null && authentication.isAuthenticated()
+                && !(authentication instanceof AnonymousAuthenticationToken)) {
+            return "redirect:/";
+        }
+        return "login";
+    }
 
     @GetMapping("/app/error")
     public ModelAndView error() {
